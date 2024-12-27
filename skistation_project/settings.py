8ccbc812836e81e27a75d43612c7537809502327
Rodrigo Locoselli <locoselli2016@gmail.com>
2024-12-27 21:38:36 +0100

    adding default database

--- skistation_project/settings.py
+++ skistation_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -92,6 +93,10 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',  # This will create the SQLite database file in your project base directory
     }
 }
+
+# If running on Heroku, override with PostgreSQL database configuration
+if 'DATABASE_URL' in os.environ:
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
