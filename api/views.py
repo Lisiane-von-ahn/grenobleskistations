e8a391f38d6b37d8666c61a5bec9a8159ce58989
rlocoselli@outlook.fr <rlocoselli@outlook.fr>
2025-03-29 08:26:34 +0100

    Adding new views

--- api/views.py
+++ api/views.py
@@ -1,6 +1,10 @@
+from django.contrib.auth.models import User
 from rest_framework import viewsets
-from .models import SkiStation, BusLine, ServiceStore, SkiCircuit
-from .serializers import SkiStationSerializer, BusLineSerializer, ServiceStoreSerializer, SkiCircuitSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from .models import SkiStation, BusLine, ServiceStore, SkiCircuit, SkiMaterialListing, Message, UserProfile
+from .serializers import (SkiStationSerializer, BusLineSerializer, ServiceStoreSerializer, 
+                          SkiCircuitSerializer, SkiMaterialListingSerializer, MessageSerializer, UserProfileSerializer)
 
 class SkiStationViewSet(viewsets.ModelViewSet):
     queryset = SkiStation.objects.all()
@@ -17,3 +21,28 @@ class ServiceStoreViewSet(viewsets.ModelViewSet):
 class SkiCircuitViewSet(viewsets.ModelViewSet):
     queryset = SkiCircuit.objects.all()
     serializer_class = SkiCircuitSerializer
+
+class SkiMaterialListingViewSet(viewsets.ModelViewSet):
+    queryset = SkiMaterialListing.objects.all()
+    serializer_class = SkiMaterialListingSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return Message.objects.filter(sender=user) | Message.objects.filter(recipient=user)
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    queryset = UserProfile.objects.all()
+    serializer_class = UserProfileSerializer
+
+    @action(detail=False, methods=['get'])
+    def me(self, request):
+        profile = UserProfile.objects.get(user=request.user)
+        serializer = self.get_serializer(profile)
+        return Response(serializer.data)
