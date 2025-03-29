17958685080ad3b8df400bdaf4b7700893436b80
rlocoselli@outlook.fr <rlocoselli@outlook.fr>
2025-03-29 08:36:50 +0100

    registering skimaterial

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,11 @@
 from rest_framework.routers import DefaultRouter
-from .views import SkiStationViewSet, BusLineViewSet, ServiceStoreViewSet, SkiCircuitViewSet
+from .views import SkiStationViewSet, BusLineViewSet, ServiceStoreViewSet, SkiCircuitViewSet,SkiMaterialListingViewSet
 
 router = DefaultRouter()
 router.register(r'skistations', SkiStationViewSet)
 router.register(r'buslines', BusLineViewSet)
 router.register(r'servicestores', ServiceStoreViewSet)
 router.register(r'skicircuits', SkiCircuitViewSet)
+router.register(r'skimaterial', SkiMaterialListingViewSet)
 
 urlpatterns = router.urls
