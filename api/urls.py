61f91f3b5520667a95d639c51916f05692c300d7
rlocoselli@outlook.fr <rlocoselli@outlook.fr>
2025-03-30 15:51:18 +0200

    adding userviewset

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,5 @@
 from rest_framework.routers import DefaultRouter
-from .views import SkiStationViewSet, BusLineViewSet, ServiceStoreViewSet, SkiCircuitViewSet,SkiMaterialListingViewSet, UserProfileViewSet
+from .views import SkiStationViewSet, BusLineViewSet, ServiceStoreViewSet, SkiCircuitViewSet,SkiMaterialListingViewSet, UserProfileViewSet, UserViewSet
 
 router = DefaultRouter()
 router.register(r'skistations', SkiStationViewSet)
@@ -8,5 +8,6 @@ router.register(r'servicestores', ServiceStoreViewSet)
 router.register(r'skicircuits', SkiCircuitViewSet)
 router.register(r'skimaterial', SkiMaterialListingViewSet)
 router.register(r'userprofile', UserProfileViewSet)
+router.register(r'userprofile', UserViewSet)
 
 urlpatterns = router.urls
