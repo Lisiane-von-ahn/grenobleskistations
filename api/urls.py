c976514fa2f5522bb7bd0d8948c6c0dc3a1d57dc
rlocoselli@outlook.fr <rlocoselli@outlook.fr>
2025-04-05 08:31:33 +0200

    adding login to url

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,9 @@
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import SkiStationViewSet, BusLineViewSet, ServiceStoreViewSet, SkiCircuitViewSet,SkiMaterialListingViewSet, UserProfileViewSet, UserViewSet
-
+from .views import (
+    SkiStationViewSet, BusLineViewSet, ServiceStoreViewSet, SkiCircuitViewSet,
+    SkiMaterialListingViewSet, UserProfileViewSet, UserViewSet, login_view
+)
 router = DefaultRouter()
 router.register(r'skistations', SkiStationViewSet)
 router.register(r'buslines', BusLineViewSet)
@@ -10,4 +13,7 @@ router.register(r'skimaterial', SkiMaterialListingViewSet)
 router.register(r'userprofile', UserProfileViewSet)
 router.register(r'userview', UserViewSet)
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('login/', login_view, name='login'),  # Add the login endpoint manually
+] + router.urls
+
